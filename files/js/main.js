da0bf0a092dd978ea132d6def183ab5cefff40cf
wpwjd169 <wpwjd169@naver.com>
2021-05-28 10:29:05 +0900

    1차 정적용 포트폴리오

---
+++ files/js/main.js
@@ -0,0 +1,77 @@
+window.onload = function() {
+    let chart = new CanvasJS.Chart("java", {
+        backgroundColor:"transparent",
+        animationEnabled: true,
+        title:{
+            text: "Java",
+            horizontalAlign: "center"
+        },
+        data: [{
+            type: "doughnut",
+            startAngle: 30,
+            //innerRadius: 60,
+            indexLabelFontSize: 17,
+            toolTipContent: "<b>{label}</b> = #percent%",
+            dataPoints: [
+                { y: 60, label:"java", color: "blue"},
+                { y: 40, color: "transparent"}
+            ]
+        }]
+        
+    });
+    chart.render();
+    
+}
+
+window.addEventListener('load', () => {
+    let chart = new CanvasJS.Chart("html", {
+        backgroundColor:"transparent",
+        animationEnabled: true,
+        title:{
+            text: "html | css | js",
+            horizontalAlign: "center"
+        },
+        data: [{
+            type: "doughnut",
+            startAngle: 30,
+            //innerRadius: 60,
+            indexLabelFontSize: 17,
+            toolTipContent: "<b>{label}</b> = #percent%",
+            dataPoints: [
+                { y: 50, label:"html", color: "green"},
+                { y: 30, label:"css", color: "blue"},
+                { y: 20, label:"js", color: "purple"}
+            ]
+        }]
+        
+    });
+    chart.render();
+    
+});
+
+window.addEventListener('load', () => {
+    let chart = new CanvasJS.Chart("oracle", {
+        backgroundColor:"transparent",
+        animationEnabled: true,
+        title:{
+            text: "Oracle",
+            horizontalAlign: "center"
+        },
+        data: [{
+            type: "doughnut",
+            startAngle: 30,
+            //innerRadius: 60,
+            indexLabelFontSize: 17,
+            toolTipContent: "<b>{label}</b> = #percent%",
+            dataPoints: [
+                { y: 30, label:"oracle", color: "black"},
+                { y: 15, label:"mybatis", color: "yellow"},
+                { y: 55, color: "transparent"}
+            ]
+        }]
+        
+    });
+    chart.render();
+    
+});
+
